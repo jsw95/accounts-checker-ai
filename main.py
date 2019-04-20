d6a1e41982aa316fe070d2600bd6adc57f4eaff9
Jack Wells <wells.jack@hotmail.co.uk>
2019-04-20 21:05:14 +0100

    df of box image data done

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.utils import *
+from src.table_finder import find_boxes
+from skimage import io
+
+
+if __name__ == "__main__":
+
+    # img = io.imread("/home/jsw/Workspace/accounts/images/account_template.jpg", as_gray=True)
+
+    img = io.imread("/home/jsw/Workspace/accounts/images/easy_template.jpg", as_gray=True)
+
+    boxes = find_boxes(img)
+
+    coords_table = return_coords_table(boxes)
+
+    print(coords_table)
--- src/table_finder.py
+++ src/table_finder.py
@@ -10,54 +10,66 @@ from skimage.morphology import closing, square
 
 
 # img = io.imread("/home/jsw/Workspace/accounts/images/account_template.jpg", as_gray=True)
-img = io.imread("/home/jsw/Workspace/accounts/images/easy_template.jpg", as_gray=True)
-
-# img = sobel(img)
-
-# apply threshold
-# thresh = scharr(img)
-thresh = threshold_otsu(img)
-# thresh = prewitt(img)
-bw = closing(img > (1.16 * thresh),  square(1))
-
-# remove artifacts connected to image border
-cleared = clear_border(bw)
-
-# label image regions
-label_image = label(cleared)
-
-fig, ax = plt.subplots(1, 2, figsize=(10, 6))
-ax[0].imshow(img, cmap='gray')
-
-box_locations = []
-
-for region in regionprops(label_image):
-    # take regions with large enough areas
-    if region.area >= 5000:
-        # draw rectangle around segmented coins
+# img = io.imread("/home/jsw/Workspace/accounts/images/easy_template.jpg", as_gray=True)
 
 
-        minr, minc, maxr, maxc = region.bbox
-        top_left_corner = (minr, minc)
-        box_locations.append(top_left_corner)
+def draw_red_boxes(img):
 
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                  fill=False, edgecolor='red', linewidth=0.5)
-        ax[0].add_patch(rect)
+    thresh = threshold_otsu(img)
+    bw = closing(img > (1.16 * thresh),  square(1))
 
+    cleared = clear_border(bw)
 
-minr, minc, maxr, maxc = (191, 897, 257, 1038)
-minr, minc, maxr, maxc = (1240, 2, 1310, 723)
+    label_image = label(cleared)
 
-box_filtered = img[minr:maxr, minc:maxc]
-
-io.imsave("box.jpg", box_filtered)
+    fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+    ax[0].imshow(img, cmap='gray')
 
 
-b = io.imread("box.jpg", as_gray=True)
+    for region in regionprops(label_image):
 
-ax[1].imshow(b)
+        if region.area >= 5000:
+            minr, minc, maxr, maxc = region.bbox
 
-ax[0].set_axis_off()
-plt.tight_layout()
-plt.show()
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                      fill=False, edgecolor='red', linewidth=0.5)
+            ax[0].add_patch(rect)
+
+
+    ax[1].imshow(img)
+
+    ax[0].set_axis_off()
+    plt.tight_layout()
+    plt.show()
+
+
+def find_boxes(img):
+    # apply threshold
+    thresh = threshold_otsu(img)
+
+    bw = closing(img > (1.16 * thresh), square(1))  # Unstable
+
+    # remove artifacts connected to image border
+    cleared = clear_border(bw)
+
+    label_image = label(cleared)
+
+    box_locations = []
+
+    for region in regionprops(label_image):
+        # take regions with large enough areas
+        if region.area >= 5000:
+            # draw rectangle around segmented coins
+
+            minr, minc, maxr, maxc = region.bbox
+
+            loc = region.centroid
+
+            box_img = img[minr:maxr, minc:maxc]
+            loc_with_img = (loc, box_img)
+            box_locations.append(loc_with_img)
+
+    return box_locations
+
+
+
